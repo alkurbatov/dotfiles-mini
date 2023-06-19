f1733c4786bf19157bc4a276f73ab6b6a342be1c
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2023-06-19 12:57:39 +0300

    🔧 (emacs) Add more tweaks
    - Configure backups.
    - Make work with buffers a bit more convenient.
    
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,10 @@
+;; Libraries load path
+;; Libraries load path
+(add-to-list 'load-path
+             (expand-file-name "lisp" user-emacs-directory))
+
+(require 'nano-sensitive)
+
 ;; Turn off menu bar, tool bar, and scroll bar.
 (if (fboundp 'menu-bar-mode)   (menu-bar-mode   -1))
 (if (fboundp 'tool-bar-mode)   (tool-bar-mode   -1))
@@ -14,4 +21,34 @@
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (add-hook 'text-mode-hook #'display-line-numbers-mode)
 
+;; Backup
+(setq backup-directory-alist '(("." . "~/.backups"))
+      make-backup-files t     ; backup of a file the first time it is saved.
+      backup-by-copying t     ; don't clobber symlinks
+      vc-make-backup-files t  ; backup version controlled files too as we don't commit on every save
+      version-control t       ; version numbers for backup files
+      delete-old-versions t   ; delete excess backup files silently
+      kept-old-versions 6     ; oldest versions to keep when a new numbered
+                              ;  backup is made (default: 2)
+      kept-new-versions 9     ; newest versions to keep when a new numbered
+                              ;  backup is made (default: 2)
+      auto-save-default t     ; auto-save every buffer that visits a file
+      auto-save-timeout 20    ; number of seconds idle time before auto-save
+                              ;  (default: 30)
+      auto-save-interval 200) ; number of keystrokes between auto-saves
+                              ;  (default: 300)
+
+;; Exclude sensitive data from backups
+(add-to-list 'auto-mode-alist '("\\.gpg$" . sensitive-mode))
+(add-to-list 'auto-mode-alist '("\\.netrc$" . sensitive-mode))
+
 (load-theme 'tsdh-dark)
+
+;; y/n for  answering yes/no questions
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Remember the last place visited in a file
+(save-place-mode 1)
+
+;; Automatically revert buffers for changed files if they don't have unsaved changes
+(global-auto-revert-mode 1)
